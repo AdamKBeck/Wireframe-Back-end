26488c41a67f599520ced588fae85d6d6b059e53
Adam Beck <akb93@case.edu>
2017-11-25 01:12:25 -0500

    Location UML to code
    Enforced singleton pattern. Setters return type altered to Unit

--- src/main/scala/Location.scala
+++ src/main/scala/Location.scala
@@ -1,3 +1,26 @@
-case class Location() {
+// Provides location information of an element on the canvas. Final, as no class should extend this
+final case class Location private(private var _x: Int = Location.DEFAULT_X_LOCATION,
+	private var _y: Int = Location.DEFAULT_Y_LOCATION) {
 
+	// Public getters
+	def x: Int = _x
+	def y: Int = _y
+
+	// Public setters
+	def x_=(coordinate: Int): Unit = {
+		x = coordinate
+	}
+	def y_=(coordinate: Int): Unit = {
+		y = coordinate
+	}
+}
+
+object Location {
+	// final static fields
+	final val DEFAULT_X_LOCATION = 0
+	final val DEFAULT_Y_LOCATION = 0
+
+	// Singleton pattern
+	private val _instance = Location()
+	def instance: Location = _instance
 }
