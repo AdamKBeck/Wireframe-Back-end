dc675272e297bf41bb1a93943335a9769e0452a1
Adam Beck <akb93@case.edu>
2017-11-28 02:44:11 -0500

    Created Slider and ScrollBar

---
+++ src/main/scala/Slider.scala
@@ -0,0 +1,28 @@
+// Provides information for functionalities for a Slider
+
+package wireframe
+
+import scala.collection.mutable.ListBuffer
+
+// Note the protected constructor similar to the Element class
+class Slider (private var _linearProperty: LinearProperty = LinearProperty.DEFAULT_LINEAR_PROPERTY,
+	private var _locked: Boolean = false,
+	private val _annotations: ListBuffer[Annotation] = ListBuffer(),
+	private var _value: Int = Bar.MINIMUM_VALUE)
+	extends Bar(_linearProperty, _locked, _annotations, _value) {
+
+}
+
+object Slider {
+	/* This "breaks abstraction badly", but how would a GUI know what it's drawing?
+	 * If I am creating a slider, the only way to know what to add to my GUI is to check this field.
+	 * Otherwise, all I know is that this is a subclass of an element, its shape and linear properties,
+	 * but none of this definitively tells me what type of element I have.
+	 *
+	 * When my Canvas returns a list of elements, I have all the linear properties. From there, the only
+	 * step to draw them on my GUI is to check this field, so I know what to draw (e.g. a JTextBox if
+	 * this field says "Box" or a JSlider if this field says "slider"
+	 */
+	final val TYPE: String = "Slider"
+}
+
