0568c75afccc4b696cdee3b581def606b1efaf99
Adam Beck <akb93@case.edu>
2017-11-28 01:13:32 -0500

    setLocation barricade method complete
    Attempts to move an element by itself, or move the whole group if the
    element is grouped

--- src/main/scala/WireframeBarricade.scala
+++ src/main/scala/WireframeBarricade.scala
@@ -13,6 +13,7 @@ final case class WireframeBarricade private() {
 object WireframeBarricade {
 
 	// TODO: log stuff when false
+	// TODO: move group along with element
 
 	// Attempts to change an element's width
 	def setWidth(element: Element, width: Int): Boolean = {
@@ -25,12 +26,70 @@ object WireframeBarricade {
 
 	// Attempts to change an element's height
 	def setHeight(element: Element, height: Int): Boolean = {
-		if (isUnlocked(element) && validHeight(element, width)) {
+		if (isUnlocked(element) && isValidHeight(element, height)) {
 			element.height = height
 			true
 		}
 		false
 	}
+
+	// Attempts to change an element's position. This also changes the group too
+	def setLocation(newX: Int, newY:Int, element: Element): Boolean = {
+		// If there are no groups that have this element, move it individually
+		if (groupWith(element).isEmpty) {
+			// Save the old element's positions
+			val oldX = element.x
+			val oldY = element.y
+
+			// Move the element to the desired location
+			element.x = newX
+			element.y = newY
+
+			// Checks if the new location is valid. If it isn't, revert back to the old location
+			if (isMoveValid(element)) {
+				true
+			}
+
+			else {
+				element.x = oldX
+				element.y = oldY
+				false
+			}
+		}
+
+		// Attempt the move the element and the rest of the group
+		else {
+			val elementsInGroup = groupWith(element).head.elements
+
+			// Store the relative move difference for "this" element
+			val xOffset = newX - element.x
+			val yOffset = newY - element.y
+
+			// Move the elements in the group too
+			//TODO: change to for loop? Does a map actually change these things?
+			elementsInGroup.map(element => element.x + xOffset)
+			elementsInGroup.map(element => element.y + yOffset)
+
+			// Checks if the new location is valid. If it isn't, rever back to the old locations
+			if (elementsInGroup.forall(element => isMoveValid(element))) {
+				true
+			}
+
+			else {
+				elementsInGroup.map(element => element.x - xOffset)
+				elementsInGroup.map(element => element.y - yOffset)
+
+				false
+			}
+		}
+	}
+
+	// Checks if an element was moved to a valid spot (in the canvas, non overlapping)
+	def isMoveValid(element: Element): Boolean = {
+		isUnlocked(element) && isValidWidth(element, element.width) && isValidHeight(element, element.height)
+	}
+
+
 
 	// Returns based on if an element is unlocked and the group its in is unlocked
 	private def isUnlocked(element: Element): Boolean = {
@@ -43,7 +102,7 @@ object WireframeBarricade {
 	 */
 	private def isUnlockedGroupContaining(element: Element): Boolean = {
 		// Finds a group (if any) that contains the element in question
-		val groupWithElement = Canvas.instance.groups.filter(group => group.elements.contains(element))
+		val groupWithElement = groupWith(element)
 
 		// Filters this group out if it's locked
 		val unlockedGroupWithElement = groupWithElement.filter(group => !group.locked)
@@ -64,7 +123,7 @@ object WireframeBarricade {
 
 	// Checks if a proposed width on an element ends up overlapping the other elements
 	private def isWidthOverlapping(element: Element, width: Int): Boolean = {
-		var otherElements = Canvas.instance.elements.filterNot(_ == element)
+		var otherElements = elementsExcept(element)
 
 		// Filter all the elements that aren't to the left of our element
 		otherElements = otherElements.filterNot(e => (e.x + e.width) < element.x)
@@ -88,7 +147,7 @@ object WireframeBarricade {
 
 	// Checks if a proposed height on an element ends up overlapping the other elements
 	private def isHeightOverlapping(element: Element, height: Int): Boolean = {
-		var otherElements = Canvas.instance.elements.filterNot(_ == element)
+		var otherElements = elementsExcept(element)
 
 		// Filter all the elements that aren't above (vertically up) from our element
 		otherElements = otherElements.filterNot(e => (e.y + e.height) < element.y)
@@ -98,6 +157,16 @@ object WireframeBarricade {
 
 		// If any elements remain in this list, they overlap with our proposed element change
 		otherElements.nonEmpty
+	}
+
+	// Helper function to return the group(s) that contains a specified element
+	private def groupWith(element: Element): List[Group] = {
+		Canvas.instance.groups.filter(group => group.elements.contains(element))
+	}
+
+	// Helper function to return all the elements on the canvas besides our current one
+	private def elementsExcept(element: Element): List[Element] = {
+		Canvas.instance.elements.filterNot(_ == element)
 	}
 
 	// Singleton implementation
