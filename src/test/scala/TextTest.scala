8ead78214f60211a7f0c8e9709a2a8f2ed8c3117
Adam Beck <akb93@case.edu>
2017-11-28 05:36:16 -0500

    TextTest added

---
+++ src/test/scala/TextTest.scala
@@ -0,0 +1,31 @@
+package wireframe
+
+import org.scalatest.FlatSpec
+
+class TextTest extends FlatSpec {
+
+	// Good data: in the text cache
+	behavior of "test"
+	it should "Nominal, return the correct text from the cache" in {
+		val paragraph = new Paragraph()
+		assert(paragraph.text == Some("BLOCKED...."))
+	}
+
+	// Bad data: not in the text cache
+	it should "Bad data, test with something not in the text cache" in {
+		val textList = new TextList(_textType = "stuff")
+		assert(textList.text == None)
+	}
+
+	behavior of "final static fields"
+	it should "hold the correct values" in {
+		assert(Text.ALIGNMENTS.contains("Left"))
+		assert(Text.ALIGNMENTS.contains("Center"))
+		assert(Text.ALIGNMENTS.contains("Right"))
+		assert(Text.ALIGNMENTS.contains("Justified"))
+
+		assert(Text.textCache("Canned") == "Lorem Ipsum....")
+		assert(Text.textCache("Blocked") == "BLOCKED....")
+	}
+
+}
