6f6fc9869ca5a23bde20bb063d9ec8619c99ecfb
Adam Beck <akb93@case.edu>
2017-12-05 05:37:24 -0500

    Lockable commented

--- src/main/scala/Lockable.scala
+++ src/main/scala/Lockable.scala
@@ -1,6 +1,25 @@
 package wireframe
 
+/**
+  * EECS 293
+  * Created by Adam Beck on 12/5/2017
+  * akb93@case.edu
+  * Version 1.0
+  *
+  * Lockable: Trait to ensure that users of this trait must implement locking
+  */
 trait Lockable {
+	/**
+	  * Gets the locked state of an object using this trait
+	  *
+	  * @return a boolean representing if an object using this trait is locked
+	  */
 	def locked: Boolean
+
+	/**
+	  * Sets the locked state of an object using this trait
+	  *
+	  * @param value the locked value, represented as a boolean, to set for an object using this trait
+	  */
 	def locked_=(value: Boolean): Unit
 }
