610cd77ff02a6825c8843d80897b6dd21c4bbf71
Adam Beck <akb93@case.edu>
2017-12-05 05:33:59 -0500

    Location commented

--- src/main/scala/Location.scala
+++ src/main/scala/Location.scala
@@ -1,18 +1,48 @@
-// Provides location information of an element on the canvas. Final, as no class should extend this
 package wireframe
 
+/**
+  * EECS 293
+  * Created by Adam Beck on 12/5/2017
+  * akb93@case.edu
+  * Version 1.0
+  *
+  * Location: A location on the canvas that an object contains
+  * Final, as nothing should extend this class
+  *
+  * @param _x the x coordinate of the location, in pixels
+  * @param _y the y coordinate of the location, in pixels
+  */
 final case class Location (private var _x: Int, private var _y: Int) {
-	// Public getters
+	/**
+	  * Gets the x-coordinate of the object using the location
+	  *
+	  * @return the x coordinate of the object using the location, in pixels
+	  */
 	def x: Int = _x
+
+	/**
+	  * Gets the y-coordinate of the object using the location
+	  *
+	  * @return the y coordinate of the object using the location, in pixels
+	  */
 	def y: Int = _y
 
-	// Public setters
+	/**
+	  * Sets the x-coordinate of the object using the location
+	  *
+	  * @param coordinate the coordinate, represented as a pixel Int, to set the object to
+	  */
 	def x_=(coordinate: Int): Unit = _x = coordinate
+
+	/**
+	  * Sets the y-coordinate of the object using the location
+	  *
+	  * @param coordinate the coordinate, represented as a pixel Int, to set the object to
+	  */
 	def y_=(coordinate: Int): Unit = _y = coordinate
 }
 
 object Location {
-	// public final static fields
-	final val DEFAULT_X_LOCATION: Int = 0
-	final val DEFAULT_Y_LOCATION: Int = 0
+	final val DEFAULT_X_LOCATION: Int = 0 // The default x-location of the object having the location, in pixels
+	final val DEFAULT_Y_LOCATION: Int = 0 // The default y-location of the object having the location, in pixels
 }
