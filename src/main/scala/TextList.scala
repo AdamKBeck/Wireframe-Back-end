0a4b722dc3a5b4ebd1e99ca7890ed4cef1e7e630
Adam Beck <akb93@case.edu>
2017-12-05 05:54:55 -0500

    TextList commented

--- src/main/scala/TextList.scala
+++ src/main/scala/TextList.scala
@@ -1,10 +1,26 @@
 // List might interfere with the List keyword so I renamed this class TextList
-// Provides information for functionalities for a TextList
 
 package wireframe
 
 import scala.collection.mutable.ListBuffer
 
+/**
+  * EECS 293
+  * Created by Adam Beck on 12/5/2017
+  * akb93@case.edu
+  * Version 1.0
+  *
+  * TextList: A type of Text that contains a text type, alignment, and paragraphs for
+  * each bullet point in the text.
+  * Final, as nothing should textend this.
+  *
+  * @param _linearProperty the linear property relating to the element
+  * @param _locked the status indicating of an element is locked
+  * @param _annotations a list of annotations relating to the element
+  * @param _textType the type of text, canned or blocked, represented as a string, the text contains
+  * @param _alignment the type of alignment for the text, left right center or justified
+  * @param _paragraphs the list of paragraphs, each paragraph is a bullet point in the text
+  */
 final class TextList (private var _linearProperty: LinearProperty = LinearProperty.DEFAULT_LINEAR_PROPERTY,
 	private var _locked: Boolean = false,
 	private val _annotations: ListBuffer[Annotation] = ListBuffer(),
@@ -13,24 +29,28 @@ final class TextList (private var _linearProperty: LinearProperty = LinearProper
 	private val _paragraphs: ListBuffer[Paragraph] = ListBuffer())
 	extends Text(_linearProperty, _locked, _annotations, _textType, _alignment) {
 
-	// Public getters
+	/**
+	  * Gets the paragraphs for the textlist
+	  *
+	  * @return a list of paragraphs, represnting the bullet points, for the textlist
+	  */
 	def paragraphs: List[Paragraph] = _paragraphs.toList // Defensive copy
 
-	// Public functions
+	/**
+	  * Adds a paragraph to the textlist
+	  *
+	  * @param paragraph the paragraph to add to the textlist
+	  */
 	def add(paragraph: Paragraph): Unit = _paragraphs += paragraph
 
+	/**
+	  * Removes a paragraph from the textlist
+	  *
+	  * @param index the index of the paragraph to remove from the textlist
+	  */
 	def remove(index: Int): Unit = _paragraphs.remove(index)
 }
 
 object TextList {
-	/* This "breaks abstraction badly", but how would a GUI know what it's drawing?
-	 * If I am creating a slider, the only way to know what to add to my GUI is to check this field.
-	 * Otherwise, all I know is that this is a subclass of an element, its shape and linear properties,
-	 * but none of this definitively tells me what type of element I have.
-	 *
-	 * When my Canvas returns a list of elements, I have all the linear properties. From there, the only
-	 * step to draw them on my GUI is to check this field, so I know what to draw (e.g. a JTextBox if
-	 * this field says "Box" or a JSlider if this field says "slider"
-	 */
-	final val TYPE: String = "TextList"
+	final val TYPE: String = "TextList" // The type of the element
 }
