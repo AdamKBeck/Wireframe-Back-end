94a724a7e7a8f69c59f7e08f227001ddca709014
Adam Beck <akb93@case.edu>
2017-12-05 05:39:49 -0500

    Logger commented

--- src/main/scala/Logger.scala
+++ src/main/scala/Logger.scala
@@ -1,13 +1,26 @@
 package wireframe
 
 import java.io._
-import java.nio.file._
 
-/* This class is used to log invalid attempts to place, resize, group, etc. elements. When a barricade branch is false,
- * the reason for the failure is logged through this class. The program keeps on running as if nothing happened (as in,
- * we never tried to place an element incorrectly in the first place) to keep failure atomicity
- */
-case class Logger private() {
+/**
+  * EECS 293
+  * Created by Adam Beck on 12/5/2017
+  * akb93@case.edu
+  * Version 1.0
+  *
+  * Logger: is used to log invalid attempts to place, resize, group, etc. elements. When a barricade branch is false,
+  * the reason for the failure is logged through this class. The program keeps on running as if nothing happened (as in,
+  * we never tried to place an element incorrectly in the first place) to keep failure atomicity
+  *
+  * Final, as nothing should extend this
+  */
+final case class Logger private() {
+	/**
+	  * Logs a message to the log file
+	  *
+	  * @param message the message to log to the log file
+	  * @param fileName the filename of the logfile to work with
+	  */
 	def log(message: String, fileName:String = "log.txt"): Unit = {
 		val pw = new PrintWriter(new FileWriter("src/" + fileName, true))
 		pw.write(message + "\n")
@@ -16,9 +29,8 @@ case class Logger private() {
 }
 
 object Logger {
-	// Singleton implementation, also delete our previous log each time the program runs
+	// Singleton implementation
 	private val _instance = Logger()
-
 	def instance = _instance
 
 }
